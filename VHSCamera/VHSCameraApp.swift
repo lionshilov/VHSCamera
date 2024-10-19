67465cfa039f9e1846ea9a4d06d1918c1eefc714
lionshilov <47785492+lionshilov@users.noreply.github.com>
2024-10-19 18:14:33 +0700

    Initialization of an empty project

---
+++ VHSCamera/VHSCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  VHSCameraApp.swift
+//  VHSCamera
+//
+//  Created by Лев Шилов on 19.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct VHSCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
